31eb789fb4c79ea01331120fde7a3c043ce506cc
bledsoef <bledsoef@berea.edu>
2024-03-23 14:59:12 -0400

    wrote logic

--- app/controllers/tasks.py
+++ app/controllers/tasks.py
@@ -1,12 +1,14 @@
 from fastapi import APIRouter, Depends, Request
 from sqlalchemy.orm import Session
 from database.connection import get_db
+from app.logic.tasks import create_new_task
 router = APIRouter()
 
 @router.post("/createTask")
 async def createTask(request: Request, db: Session = Depends(get_db)):
     request_data = await request.json()
     try:
+        create_new_task(db, request_data)
         return {"message": "Task created"}
     except Exception as e:
         print(e)
