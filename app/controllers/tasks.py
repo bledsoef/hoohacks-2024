08a896a3bf7df9d9d03e7545642cf3393a5de8e2
hoerstl <hoerstl@berea.edu>
2024-03-23 14:40:42 -0400

    Started writing queries for get task

--- app/controllers/tasks.py
+++ app/controllers/tasks.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends, Request
 from sqlalchemy.orm import Session
 from database.connection import get_db
+from app.models.models import Task, User
+import json
 router = APIRouter()
 
 @router.post("/createTask")
@@ -16,6 +18,10 @@ async def createTask(request: Request, db: Session = Depends(get_db)):
 @router.get("/getTasksForUser")
 async def getSentRecsForUser(username: str, db: Session = Depends(get_db)):
     try:        
+        requestingUser = db.query(User).filter(User.username==username)
+
+        tasksForUser = [db.query(Task).filter(Task.user == username)]
+        [entry.__dict__ for entry in db.query(Rec).filter(Rec.sentTo == user_id, Rec.status == 'pending').all()]
         return "tasks go here"
     except Exception as e:
         print(e)
--- app/models/models.py
+++ app/models/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Boolean, Column, Integer, String, Date, ForeignKey
 from sqlalchemy.orm import relationship
 from database.connection import Base
+
 class User(Base):
     __tablename__ = "users"
     
