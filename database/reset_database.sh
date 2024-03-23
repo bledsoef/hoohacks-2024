cbc3680f330218394742e085a36b4bd99e19162c
bledsoef <bledsoef@berea.edu>
2024-03-23 13:55:46 -0400

    fixed database script

--- database/reset_database.sh
+++ database/reset_database.sh
@@ -1,9 +1,9 @@
 echo "-----MAKE SURE YOU ARE IN THE ROOT DIRECTORY AND RUNNING THIS SCRIPT WITH ./database/reset_database.sh \n"
 
 echo "Dropping databases"
-mysql -u root -proot --execute="DROP DATABASE \`spotify_rex\`;"
+mysql -u root -proot --execute="DROP DATABASE \`hoohacks_2024\`;"
 
 echo "Recreating databases and users"
-mysql -u root -proot --execute="CREATE DATABASE IF NOT EXISTS \`spotify_rex\`;"
+mysql -u root -proot --execute="CREATE DATABASE IF NOT EXISTS \`hoohacks_2024\`;"
 
 python3 ./database/reset_database.py
